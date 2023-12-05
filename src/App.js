5cd80217d77e4477f6ee00711fdc0c22f4885860
Masavi <mauriciosaavedra2005@gmail.com>
2023-12-05 13:37:04 -0600

    refactor: change dog photo styling, add text at the bottom

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
         <h1>Welcome to the LHL June 12th 2023 Cohort website!</h1>
         <p>Here is a picture of my dog</p>
         <img src="https://mausvil.com/assets/jensen.jpg" alt="jensen the hotdog" />
+        <p>This change was added after the github actions config!</p>
       </header>
     </div>
   );
