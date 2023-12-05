ab1e6a968d3e007cdf44f8fad2c4859b88891a2f
Masavi <mauriciosaavedra2005@gmail.com>
2023-12-05 12:26:57 -0600

    feat: add jensen the dog to the homepage, for testing purposes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to the LHL June 12th 2023 Cohort website!</h1>
+        <p>Here is a picture of my dog</p>
+        <img src="https://mausvil.com/assets/jensen.jpg" alt="jensen the hotdog" />
       </header>
     </div>
   );
